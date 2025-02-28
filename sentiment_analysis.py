ee7339b3db725867fd9bae1d46ed911e1286c249
BrianLi808 <brian.li.social@gmail.com>
2025-02-27 23:29:18 -0800

    Rough completion of sentiment analysis.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,1 +1,16 @@
 # sentiment_analysis.py
+# Author: Brian Li
+from textblob import TextBlob
+
+def sentiment_analysis(mood):
+    sentiment = TextBlob(mood).sentiment.polarity
+    if sentiment > 0.5:
+        return "very happy"
+    elif 0.1 < sentiment <= 0.5:
+        return "happy"
+    elif 0.1 >= sentiment >= -0.1:
+        return "neutral"
+    elif -0.1 > sentiment >= -0.5:
+        return "negative"
+    else: # -0.5 > sentiment > -1.0
+        return "very negative"

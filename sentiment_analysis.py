5d54ca450ca9dfb6fc1ff99a468fd9bd1f119dd9
BrianLi808 <brian.li.social@gmail.com>
2025-03-05 17:50:18 -0800

    Completion of sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,7 +2,13 @@
 # Author: Brian Li
 from textblob import TextBlob
 
-def sentiment_analysis(mood):
+genres_very_happy = ["action", "adventure", "comedy", "family", "fantasy", "musical", "romance"]
+genres_happy = ["adventure", "game-show", "fantasy", "sport"]
+genres_neutral = ["biography", "documentary", "news", "talk-show", "western"]
+genres_negative = ["mystery", "crime", "film-noir", "thriller"]
+genres_very_negative = ["adult", "crime", "horror", "film-noir", "thriller", "war"]
+
+def sentiment_analysis(mood) -> str:
     sentiment = TextBlob(mood).sentiment.polarity
     if sentiment > 0.5:
         return "very happy"
@@ -14,3 +20,13 @@ def sentiment_analysis(mood):
         return "negative"
     else: # -0.5 > sentiment > -1.0
         return "very negative"
+
+def set_genre(mood: str) -> list:
+    if mood == "very happy":
+        return genres_very_happy
+    if mood == "happy":
+        return genres_happy
+    if mood == "neutral":
+        return genres_neutral
+    if mood == "negative":
+        return genres_negative

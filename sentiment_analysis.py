83692baa3947e480e553a55518b9d9874c743863
BrianLi808 <brian.li.social@gmail.com>
2025-03-07 18:11:19 -0800

    Appropriate variable renaming

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -12,9 +12,9 @@ from textblob import TextBlob
 def sentiment_analysis(mood: str) -> str:
     sentiment = TextBlob(mood).sentiment.polarity
     if sentiment > 0.5:
-        return "very happy"
+        return "very positive"
     elif 0.1 < sentiment <= 0.5:
-        return "happy"
+        return "positive"
     elif 0.1 >= sentiment >= -0.1:
         return "neutral"
     elif -0.1 > sentiment >= -0.5:
@@ -22,16 +22,17 @@ def sentiment_analysis(mood: str) -> str:
     else: # -0.5 > sentiment > -1.0
         return "very negative"
 
-
 # The function selects a list of movie genres based on the given mood category.
 # mood (str) argument is a string representing a mood description or phrase
 # The function returns a list of movie genres corresponding to the input mood, given from imported data.
 def set_genre(mood: str) -> list:
-    if mood == "very happy":
-        return data.genres_very_happy
-    if mood == "happy":
-        return data.genres_happy
+    if mood == "very positive":
+        return data.genres_very_positive
+    if mood == "positive":
+        return data.genres_positive
     if mood == "neutral":
         return data.genres_neutral
     if mood == "negative":
         return data.genres_negative
+    if mood == "very negative":
+        return data.genres_very_negative

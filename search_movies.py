b9d8b58efd019f0563e126669a459441f467b060
nataliecartwright <cartwrightnatalie5@gmail.com>
2025-03-09 15:18:47 -0700

    Fifth Commit

--- search_movies.py
+++ search_movies.py
@@ -6,19 +6,13 @@ ia = imdb.IMDb()
 
 
 #This searches the database of movies for a movie in the specified genre
-#Genre is a string that is inputted by the user that they want to search for
+#keyword is a string that is inputted by the user that they want to search for
 #the function returns a list of movies that are in the given genre
-def search_by_genre(genre: str) -> list[str]:
+def search_by_keyword(keyword: str) -> list[str]:
     #List of movie recommendations
-    keyword = genre
-    search = ia.get_keyword(keyword)
-    return search
-#def search_by_director(director: str) -> list[str]:
- #   movies = []
-  #  keyword = director
-   # search = ia.get_keyword(keyword)
-    #movies.append(search)
-    #return movies
+    movies = ia.search_keyword(keyword)
+    return [movies['title'] for movie in movies]
+
 
 
 #A function that search through the IMDB database for movies with the given title
@@ -31,34 +25,54 @@ def search_by_title(title: str) -> list[str]:
 #A function that searches for movies based on their ratings
 #rating is an integer given by the user that they want to search for
 #The function returns a list of movies that all have the specified rating given by the user
-def search_by_rating(rating: int) -> list[str]:
-    movies = []
-    keyword = rating
-    #Add if rating is above a threshold and search for all movies greater/equal to threshold
+def search_by_rating(min_rating: int) -> list[str]:
+    top_movies = ia.get_top250_movies()
+    return [movie['title'] for movie in top_movies if movie.get('rating',0) >= min_rating]
 
 
 #A function that searches for movies depending on a specified length
 #Length is an integer given by the user that they are searching for
 #The function returns a list of movies all of that specified length
 def search_by_length(length: int):
-    #Same as above but use a time aspect instead of genre
+    movies = ia.get_top250_movies()
+    return [movie['title'] for movie in movies if movie.get('runtimes') and int(movie['runtimes'][0]) == length]
 
 
-#A function that searches for movies based on if they contain the specified person or not
-#Person is an inputted string that the users wants to search for
-#The function returns a list of movies containing the specified person
+# This search the database for movies with a specified director
+# director is string that is inputted by the user that they want to search for
+# This function returns a list of movies containing the inputted director
+def search_by_director(director: str) -> list[str]:
+    search = ia.search_person(director)
+    if not search:
+        return []
+    director_id = search[0].personID
+    director_info = ia.get_person_filmography(director_id)
+
+    if 'director' in director_info['data']['filmography']:
+        return [movie['title'] for movie in director_info['data']['filmography']['director']]
+    else:
+        return []
+
+
+# A function that searches for movies based on if they contain the specified person or not
+# Person is an inputted string that the users wants to search for
+# The function returns a list of movies containing the specified person
 def search_by_person(person: str) -> list[str]:
-    movies = []
     people = ia.search_person(person)
-    for person in people:
-    movies.append(person)
+    if not people:
+        return []
+
+    person_id = people[0].personID
+    person_info = ia.get_person(person_id)
+
+    if 'filmography' in person_info.data:
+        filmography = person_info['filmography']
+        movies = []
+        for category in filmography:
+            for movie in filmography[category]:
+                movies.append(movie['title'])
+            return movies
+    else:
+        return []
 
 
-#Testing the functions
-
-
-#Works
-print(search_by_title("Avengers"))
-
-#In progress
-print(search_by_genre("dystopian"))

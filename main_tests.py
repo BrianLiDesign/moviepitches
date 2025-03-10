42d9a2f6716a4e04f06b3c095677cd7425873ea2
nataliecartwright <cartwrightnatalie5@gmail.com>
2025-03-09 21:29:35 -0700

    Sixth Commit

--- main_tests.py
+++ main_tests.py
@@ -1,5 +1,7 @@
 # main_tests.py
 import unittest
+
+import search_movies
 import sentiment_analysis
 # import search_movies
 
@@ -63,3 +65,66 @@ class TestSetGenre(unittest.TestCase):
     def test_set_genre_very_negative(self):
         result = sentiment_analysis.set_genre("very negative")
         expected = ["adult", "crime", "horror", "film-noir", "thriller", "war"]
+
+
+#Tests for search_movies
+class TestSearchMovies(unittest.TestCase):
+    #Tests the search_by_keyword function
+    def test_search_by_keyword(self):
+        result = search_movies.search_by_keyword("cars")
+        expected = ['Cars',
+ 'Cars 3',
+ 'Cars 2',
+ 'Z Cars',
+ 'Cars on the Road',
+ 'Used Cars',
+ 'Riding in Cars with Boys',
+ 'Comedians in Cars Getting Coffee',
+ 'Counting Cars',
+ 'The Cars That Ate Paris',
+ 'Hot Cars',
+ 'Cars of the Revolution',
+ 'Old Men in New Cars',
+ 'Stealing Cars',
+ 'Chasing Classic Cars',
+ 'Cars: The Video Game',
+ 'The Cars: You Might Think',
+ 'King of Cars',
+ "Cars 3: Cars D'oeuvres",
+ 'Cars']
+        self.assertEqual(result, expected)
+
+        #Tests the search_by_title function
+    def test_search_by_title(self):
+        result = search_movies.search_by_title("Jurassic World")
+        expected = ['Jurassic World: Rebirth',
+ 'Jurassic World',
+ 'Jurassic World: Dominion',
+ 'Jurassic World: Fallen Kingdom',
+ 'The Lost World: Jurassic Park',
+ 'Jurassic World: Chaos Theory',
+ 'Jurassic World: Camp Cretaceous',
+ 'Jurassic World Dominion: The Prologue',
+ 'Lego Jurassic World: Legend of Isla Nublar',
+ 'Jurassic World: The Ride',
+ 'Jurassic World: Blue',
+ 'Lego Jurassic World: The Indominus Escape',
+ 'Jurassic World: Fallen Kingdom - The Trailer Rescue',
+ 'Jurassic World Evolution 2',
+ 'Lego Jurassic World: The Secret Exhibit',
+ 'Lego Jurassic World: Double Trouble',
+ 'Exodus: Jurassic World Fan Film',
+ 'Lego Jurassic World',
+ 'Jurassic World Evolution',
+ 'From Jurassic Park to Jurassic World: Greatest Moments']
+        self.assertEqual(result, expected)
+
+    def test_search_by_rating(self):
+        result = search_movies.search_by_rating(5)
+        expected = []
+        self.assertEqual(result, expected)
+
+    def test_search_by_director(self):
+        result = search_movies.search_by_director("Steve Spielberg")
+        expected = ['Steve Spielberg',]
+        self.assertEqual(result, expected)
--- search_movies.py
+++ search_movies.py
@@ -10,9 +10,10 @@ ia = imdb.IMDb()
 #the function returns a list of movies that are in the given genre
 def search_by_keyword(keyword: str) -> list[str]:
     #List of movie recommendations
-    movies = ia.search_keyword(keyword)
-    return [movies['title'] for movie in movies]
+    movies = ia.search_movie(keyword)
 
+    movie_titles = [str(movie) for movie in movies]
+    return movie_titles
 
 
 #A function that search through the IMDB database for movies with the given title
@@ -20,14 +21,18 @@ def search_by_keyword(keyword: str) -> list[str]:
 #The function returns both the movie ID and movie title
 def search_by_title(title: str) -> list[str]:
     movies = ia.search_movie(title)
-    return movies
+    movie_titles = [str(movie) for movie in movies]
+    return movie_titles
 
 #A function that searches for movies based on their ratings
 #rating is an integer given by the user that they want to search for
 #The function returns a list of movies that all have the specified rating given by the user
 def search_by_rating(min_rating: int) -> list[str]:
     top_movies = ia.get_top250_movies()
-    return [movie['title'] for movie in top_movies if movie.get('rating',0) >= min_rating]
+    for movie in top_movies[:3]:
+        print(movie)
+
+print(search_by_rating(5))
 
 
 #A function that searches for movies depending on a specified length
@@ -41,17 +46,10 @@ def search_by_length(length: int):
 # This search the database for movies with a specified director
 # director is string that is inputted by the user that they want to search for
 # This function returns a list of movies containing the inputted director
-def search_by_director(director: str) -> list[str]:
-    search = ia.search_person(director)
-    if not search:
-        return []
-    director_id = search[0].personID
-    director_info = ia.get_person_filmography(director_id)
-
-    if 'director' in director_info['data']['filmography']:
-        return [movie['title'] for movie in director_info['data']['filmography']['director']]
-    else:
-        return []
+def search_by_director(director: str) -> str:
+    info = ia.get_person_infoset()
+    for element in info:
+        print(element)
 
 
 # A function that searches for movies based on if they contain the specified person or not
@@ -74,5 +72,4 @@ def search_by_person(person: str) -> list[str]:
             return movies
     else:
         return []
-
 

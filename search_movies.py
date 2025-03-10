d34b1872591b4d1a711bff50d8706a321a277b4c
nataliecartwright <cartwrightnatalie5@gmail.com>
2025-03-09 21:58:40 -0700

    Seventh Commit

--- search_movies.py
+++ search_movies.py
@@ -1,31 +1,47 @@
 # search_movies.py
 # Author: Natalie Cartwright
 
-import imdb
-ia = imdb.IMDb()
+
 
 
 #This searches the database of movies for a movie in the specified genre
 #keyword is a string that is inputted by the user that they want to search for
 #the function returns a list of movies that are in the given genre
-def search_by_keyword(keyword: str) -> list[str]:
+def search_by_keyword(genre: str, movie_database) -> list[str]:
     #List of movie recommendations
-    movies = ia.search_movie(keyword)
+    movies = []
+    for movie in movie_database:
+        if movie.genre.lower() == genre.lower():
+            movies.append(movie)
+    return movies
 
-    movie_titles = [str(movie) for movie in movies]
-    return movie_titles
+
 
 
 #A function that search through the IMDB database for movies with the given title
 #title is a string and is the title of the movie that the user is searching for
 #The function returns both the movie ID and movie title
-def search_by_title(title: str) -> list[str]:
-    movies = ia.search_movie(title)
-    movie_titles = [str(movie) for movie in movies]
-    return movie_titles
+def search_by_title(title: str, movie_database) -> list[str]:
+    movies = []
+    for movie in movie_database:
+        if movie.title.lower() == title.lower():
+            movies.append(movie)
+    return movies
 
 
-def search_by_rating(number: int) -> list[str]:
+def search_by_rating(number: int, movie_database) -> list[str]:
+    movies = []
+    for movie in movie_database:
+        if movie.rating >= number:
+            movies.append(movie)
+    return movies
+
+def search_by_length(length: int, movie_database) -> list[str]:
+    movies = []
+    for movie in movie_database:
+        if movie.length >= length:
+            movies.append(movie)
+    return movies
 
 
 
